f370c40cbbbd5f6369a022277c6a057fda7892b6
Patrik Molnár <patrik.molnar@zooshgroup.com>
2023-07-17 11:58:11 +0200

    introducing axios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./graphql/schema.js";
 import router from "./resolver/router.js";
+import { fetchPopular } from "./axios/tmdb/movies.js";
 
 const app = new express();
 
@@ -13,5 +14,11 @@ app.use(
     graphiql: true,
   })
 );
+
+app.get("/", async (req, res) => {
+  console.log(await fetchPopular());
+  res.send("Hello World!");
+});
+console.log("Server running at http://localhost:666/graphql");
 
 app.listen(666);
